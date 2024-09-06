3990a594c320512ea0726b25ed80e4d6c76bf496
kkratterf <kkratterf@gmail.com>
2024-09-06 14:11:41 +0200

    new hp

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -15,6 +15,15 @@ const config: Pick<Config, "presets"> = {
       theme: {
         extend: {
           ...sharedConfig?.theme?.extend,
+          animation: {
+            grid: "grid 15s linear infinite",
+          },
+          keyframes: {
+            grid: {
+              "0%": { transform: "translateY(-50%)" },
+              "100%": { transform: "translateY(0)" },
+            },
+          },
         },
       },
     },
