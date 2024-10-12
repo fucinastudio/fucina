0d9e670e7b0456ef259300bf214d06b1d7b9cfea
kkratterf <kkratterf@gmail.com>
2024-10-12 18:09:03 +0100

    clean up components

--- apps/web/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export function getDocument(node: Element | null) {
-  return node?.ownerDocument || document;
-}
-
-export function getWindow(value: any) {
-  return getDocument(value).defaultView || window;
-}
